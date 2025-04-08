0002e2c1363304a68ad51f429265b13f11b7a16a
mj023 <max.jahn45@gmail.com>
2025-04-08 15:59:40 +0200

    Add test example

--- src/_gettsim_tests/test_arbeitslosengeld_2.py
+++ src/_gettsim_tests/test_arbeitslosengeld_2.py
@@ -1,7 +1,9 @@
 import dags.tree as dt
+import numpy as np
 import pytest
 from numpy.testing import assert_array_almost_equal
 
+from _gettsim.arbeitslosengeld_2.group_by_ids import bg_id, eg_id, fg_id
 from _gettsim_tests._helpers import cached_set_up_policy_environment
 from _gettsim_tests._policy_test_utils import PolicyTest, load_policy_test_data
 from ttsim import compute_taxes_and_transfers
@@ -26,3 +28,43 @@ def test_arbeitslosengeld_2(test: PolicyTest):
         flat_result.values(), flat_expected_output_tree.values()
     ):
         assert_array_almost_equal(result, expected, decimal=2)
+
+
+@pytest.fixture
+def ordered_data():
+    data = {
+        "p_id_einstandspartner": np.asarray([2, -1, 0]),
+        "p_id": np.asarray([0, 1, 2]),
+        "hh_id": np.asarray([0, 0, 0]),
+        "alter": np.asarray([39, 14, 42]),
+        "familie__p_id_elternteil_1": np.asarray([-1, 2, -1]),
+        "familie__p_id_elternteil_2": np.asarray([-1, -1, -1]),
+        "eigenbedarf_gedeckt": np.asarray([False, False, False]),
+    }
+    return data
+
+
+def test_fg_id(ordered_data):
+    fg_input = ordered_data.copy()
+    fg_input.pop("eigenbedarf_gedeckt")
+    fg_ids = fg_id(**fg_input)
+    np.testing.assert_equal(fg_ids, np.asarray([0, 0, 0]))
+
+
+def test_bg_id(ordered_data):
+    fg_input = ordered_data.copy()
+    fg_input.pop("eigenbedarf_gedeckt")
+    bg_ids = bg_id(
+        fg_id=fg_id(**fg_input),
+        eigenbedarf_gedeckt=ordered_data["eigenbedarf_gedeckt"],
+        alter=ordered_data["alter"],
+    )
+    np.testing.assert_equal(bg_ids, np.asarray([0, 0, 0]))
+
+
+def test_eg_id(ordered_data):
+    eg_ids = eg_id(
+        p_id_einstandspartner=ordered_data["p_id_einstandspartner"],
+        p_id=ordered_data["p_id"],
+    )
+    np.testing.assert_equal(eg_ids, np.asarray([0, 1, 0]))
