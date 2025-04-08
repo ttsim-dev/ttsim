b55486d5962be91303fd3819bb1a12e9cd1281c1
Marvin Immesberger <immesberger@posteo.de>
2025-04-08 09:28:05 +0200

    Rename to METTSIM.

---
+++ tests/ttsim/mettsim/config.py
@@ -0,0 +1,5 @@
+"""Middle-Earth Taxes and Transfers Simulator.
+
+TTSIM specification for testing purposes. Taxes and transfer names follow a law-to-code
+approach based on the Gondorian tax code.
+"""
