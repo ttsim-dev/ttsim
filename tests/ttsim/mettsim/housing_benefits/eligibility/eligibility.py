a0590235e7f78273a28d98ccab2a760936c0c21a
Marvin Immesberger <immesberger@posteo.de>
2025-04-16 15:02:09 +0200

    Move METTSIM housing benefit eligibility to family level instead of sp level.

--- tests/ttsim/mettsim/housing_benefits/eligibility/eligibility.py
+++ tests/ttsim/mettsim/housing_benefits/eligibility/eligibility.py
@@ -12,26 +12,26 @@ Policy regime starting in 2020:
 from ttsim import AggType, agg_by_group_function, policy_function
 
 
-@agg_by_group_function(agg_type=AggType.SUM)
-def number_of_children_fam(payroll_tax__fam_id: int, child: bool) -> int:
-    """The number of children in the family."""
+@agg_by_group_function(agg_type=AggType.SUM, start_date="2020-01-01")
+def number_of_adults_fam(payroll_tax__fam_id: int, adult: bool) -> int:
+    """The number of adults in the family."""
 
 
 @agg_by_group_function(agg_type=AggType.COUNT)
-def number_of_individuals_sp(payroll_tax__sp_id: int) -> int:
-    """The number of individuals in a spousal relationship."""
+def number_of_individuals_fam(payroll_tax__fam_id: int) -> int:
+    """The number of individuals in the family."""
 
 
 @policy_function(end_date="2019-12-31", leaf_name="requirement_fulfilled_fam")
 def requirement_fulfilled_fam_not_considering_children(
-    housing_benefits__income__amount_m_sp: float,
-    number_of_individuals_sp: int,
+    housing_benefits__income__amount_m_fam: float,
+    number_of_adults_fam: int,
     housing_benefits_params: dict,
 ) -> bool:
     return (
-        housing_benefits__income__amount_m_sp
+        housing_benefits__income__amount_m_fam
         < housing_benefits_params["eligibility"]["subsistence_income_per_spouse_m"]
-        * number_of_individuals_sp
+        * number_of_adults_fam
     )
 
 
@@ -39,31 +39,36 @@ def requirement_fulfilled_fam_not_considering_children(
 def requirement_fulfilled_fam_considering_children(
     housing_benefits__income__amount_m_fam: float,
     housing_benefits_params: dict,
-    number_of_children_considered: int,
-    number_of_individuals_sp: int,
+    number_of_family_members_considered_fam: int,
 ) -> bool:
     return housing_benefits__income__amount_m_fam < (
-        housing_benefits_params["eligibility"]["subsistence_income_per_spouse_m"]
-        * number_of_individuals_sp
-        + housing_benefits_params["eligibility"]["subsistence_income_per_child_m"]
-        * number_of_children_considered
+        housing_benefits_params["eligibility"]["subsistence_income_per_individual_m"]
+        * number_of_family_members_considered_fam
     )
 
 
 @policy_function(start_date="2020-01-01")
-def number_of_children_considered(
-    number_of_children_fam: int,
+def number_of_family_members_considered_fam(
+    number_of_individuals_fam: int,
     housing_benefits_params: dict,
 ) -> int:
     return min(
-        number_of_children_fam,
-        housing_benefits_params["eligibility"]["max_number_of_children"],
+        number_of_individuals_fam,
+        housing_benefits_params["eligibility"]["max_number_of_family_members"],
     )
 
 
-@policy_function(start_date="2020-01-01")
+@policy_function()
 def child(
     age: int,
     housing_benefits_params: dict,
 ) -> bool:
     return age <= housing_benefits_params["max_age_children"]
+
+
+@policy_function()
+def adult(
+    age: int,
+    housing_benefits_params: dict,
+) -> bool:
+    return age > housing_benefits_params["max_age_children"]
--- tests/ttsim/mettsim/housing_benefits/income/income.py
+++ tests/ttsim/mettsim/housing_benefits/income/income.py
@@ -11,5 +11,9 @@ from ttsim import RoundingSpec, policy_function
 def amount_m(
     payroll_tax__income__gross_wage_m: float,
     payroll_tax__amount_m: float,
+    housing_benefits__eligibility__child: bool,
 ) -> float:
-    return payroll_tax__income__gross_wage_m - payroll_tax__amount_m
+    if housing_benefits__eligibility__child:
+        return 0.0
+    else:
+        return payroll_tax__income__gross_wage_m - payroll_tax__amount_m
