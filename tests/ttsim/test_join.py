66ba24ece6bf5d47c2353d695e028df522c2549d
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-04-17 06:55:14 +0200

    Use ttsim naming convention for np, second file.

--- tests/ttsim/test_join.py
+++ tests/ttsim/test_join.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ttsim.config import numpy_or_jax
+from ttsim.config import numpy_or_jax as np
 from ttsim.shared import join
 
 
@@ -8,43 +8,43 @@ from ttsim.shared import join
     "foreign_key, primary_key, target, value_if_foreign_key_is_missing, expected",
     [
         (
-            numpy_or_jax.array([1, 2, 3]),
-            numpy_or_jax.array([1, 2, 3]),
-            numpy_or_jax.array([1, 2, 3]),
+            np.array([1, 2, 3]),
+            np.array([1, 2, 3]),
+            np.array([1, 2, 3]),
             4,
-            numpy_or_jax.array([1, 2, 3]),
+            np.array([1, 2, 3]),
         ),
         (
-            numpy_or_jax.array([3, 2, 1]),
-            numpy_or_jax.array([1, 2, 3]),
-            numpy_or_jax.array([1, 2, 3]),
+            np.array([3, 2, 1]),
+            np.array([1, 2, 3]),
+            np.array([1, 2, 3]),
             4,
-            numpy_or_jax.array([3, 2, 1]),
+            np.array([3, 2, 1]),
         ),
         (
-            numpy_or_jax.array([1, 1, 1]),
-            numpy_or_jax.array([1, 2, 3]),
-            numpy_or_jax.array([1, 2, 3]),
+            np.array([1, 1, 1]),
+            np.array([1, 2, 3]),
+            np.array([1, 2, 3]),
             4,
-            numpy_or_jax.array([1, 1, 1]),
+            np.array([1, 1, 1]),
         ),
         (
-            numpy_or_jax.array([-1]),
-            numpy_or_jax.array([1]),
-            numpy_or_jax.array([1]),
+            np.array([-1]),
+            np.array([1]),
+            np.array([1]),
             4,
-            numpy_or_jax.array([4]),
+            np.array([4]),
         ),
     ],
 )
 def test_join(
-    foreign_key: numpy_or_jax.ndarray,
-    primary_key: numpy_or_jax.ndarray,
-    target: numpy_or_jax.ndarray,
+    foreign_key: np.ndarray,
+    primary_key: np.ndarray,
+    target: np.ndarray,
     value_if_foreign_key_is_missing: int,
-    expected: numpy_or_jax.ndarray,
+    expected: np.ndarray,
 ):
-    assert numpy_or_jax.array_equal(
+    assert np.array_equal(
         join(foreign_key, primary_key, target, value_if_foreign_key_is_missing),
         expected,
     )
