0c7320447d3f8301aa1372f23f8e3c587e129d37
Marvin Immesberger <immesberger@posteo.de>
2025-04-25 14:23:41 +0200

    Draft quickrun function.

--- src/_gettsim/interface.py
+++ src/_gettsim/interface.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from _gettsim.config import FOREIGN_KEYS, RESOURCE_DIR, SUPPORTED_GROUPINGS
+from _gettsim.config import RESOURCE_DIR, SUPPORTED_GROUPINGS
 from ttsim import (
     compute_taxes_and_transfers,
     create_data_tree,
@@ -11,12 +11,64 @@ from ttsim.typing import NestedDataDict, NestedInputToSeriesNameDict
 
 def quickrun(
     date: str,
-    df: pd.DataFrame,
-    input_tree_to_column_name_mapping: NestedInputToSeriesNameDict,
+    input_tree_to_column_map: NestedInputToSeriesNameDict,
     targets_tree: NestedDataDict,
+    df: pd.DataFrame | None = None,
 ) -> NestedDataDict:
-    """Quickly compute taxes and transfers for a given date."""
-    data_tree = create_data_tree(df, input_tree_to_column_name_mapping)
+    """Compute taxes and transfers.
+
+    Args:
+        date:
+            The date to compute taxes and transfers for. The date determines the policy
+            environment for which the taxes and transfers are computed.
+        input_tree_to_column_map:
+            A nested dictionary that maps GETTSIM's expected input structure to the data
+            provided by the user. Keys are strings that provide a path to an input.
+
+            Values can be:
+
+            - Strings that reference column names in the DataFrame (when df is provided)
+            - pandas Series objects
+            - Numeric or boolean values (which will be used directly if df is None, or
+              broadcast to match the DataFrame length if df is provided)
+        targets_tree:
+            The targets tree.
+        df:
+            The DataFrame containing the data.
+
+
+    Examples:
+    --------
+    >>> input_tree_to_column_map = {
+    ...     "einkommensteuer": {
+    ...         "gemeinsam_veranlagt": "joint_taxation",
+    ...         "einkünfte": {
+    ...             "aus_nichtselbstständiger_arbeit": {
+    ...                 "bruttolohn_m": "gross_wage_m",
+    ...             },
+    ...         },
+    ...     },
+    ...     "alter": 30,
+    ...     "geburtsjahr": pd.Series([1990, 1990, 1990]),
+    ...     "p_id": pd.Series([0, 1, 2]),
+    ... }
+    >>> df = pd.DataFrame(
+    ...     {
+    ...         "gross_wage_m": [1000, 2000, 3000],
+    ...         "joint_taxation": [True, True, False],
+    ...     }
+    ... )
+    >>> quickrun(
+    ...     date="2024-01-01",
+    ...     input_tree_to_column_map=input_tree_to_column_map,
+    ...     targets_tree=targets_tree,
+    ...     df=df,
+    ... )
+    """
+    data_tree = create_data_tree(
+        input_tree_to_column_map=input_tree_to_column_map,
+        df=df,
+    )
     policy_environment = set_up_policy_environment(
         date=date,
         resource_dir=RESOURCE_DIR,
@@ -25,8 +77,8 @@ def quickrun(
         data_tree=data_tree,
         environment=policy_environment,
         targets_tree=targets_tree,
-        foreign_keys=FOREIGN_KEYS,
         supported_groupings=SUPPORTED_GROUPINGS,
         rounding=True,
         debug=False,
+        jit=False,
     )
