83a4d16945ddf4cf36f35a9ceddb7575556a4ef2
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-04-28 10:32:41 +0200

    Replace expected result calculation via legacy version by pre-calculated values.

--- tests/ttsim/test_piecewise_polynomial.py
+++ tests/ttsim/test_piecewise_polynomial.py
@@ -80,101 +80,14 @@ def test_get_piecewise_parameters_all_intercepts_supplied():
 
 
 def test_piecewise_polynomial(eink_st_params):
-    x_linspace = np.linspace(100, 100_000, 100)
+    x = np.array([-1_000, 1_000, 10_000, 30_000, 100_000, 1_000_000])
+    expected = np.array([0.0, 0.0, 246.53, 10551.65, 66438.2, 866518.64])
 
-    y = piecewise_polynomial(
-        x=x_linspace,
+    actual = piecewise_polynomial(
+        x=x,
         thresholds=eink_st_params["thresholds"],
         rates=eink_st_params["rates"],
         intercepts_at_lower_thresholds=eink_st_params["intercepts_at_lower_thresholds"],
         rates_multiplier=2,
     )
-    y_legacy = numpy.array(
-        [
-            piecewise_polynomial_legacy(
-                x=numpy.array(xi),
-                thresholds=numpy.array(eink_st_params["thresholds"]),
-                rates=numpy.array(eink_st_params["rates"]),
-                intercepts_at_lower_thresholds=numpy.array(
-                    eink_st_params["intercepts_at_lower_thresholds"]
-                ),
-                rates_multiplier=2,
-            )
-            for xi in x_linspace
-        ]
-    )
-    numpy.testing.assert_allclose(numpy.array(y), y_legacy, rtol=1e-6)
-
-
-def piecewise_polynomial_legacy(
-    x, thresholds, rates, intercepts_at_lower_thresholds, rates_multiplier=None
-):
-    """Calculate value of the piecewise function at `x`.
-
-    Parameters
-    ----------
-    x : pd.Series
-        Series with values which piecewise polynomial is applied to.
-    thresholds : numpy.array
-                A one-dimensional array containing the thresholds for all intervals.
-    rates : numpy.ndarray
-            A two-dimensional array where columns are interval sections and rows
-            correspond to the nth polynomial.
-    intercepts_at_lower_thresholds : numpy.ndarray
-        The intercepts at the lower threshold of each interval.
-    rates_multiplier : pd.Series, float
-                       Multiplier to create individual or scaled rates. If given and
-                       not equal to 1, the function also calculates new intercepts.
-
-    Returns
-    -------
-    out : float
-        The value of `x` under the piecewise function.
-
-    """
-    num_intervals = len(thresholds) - 1
-    degree_polynomial = rates.shape[0]
-
-    # Check in which interval each individual is. The thresholds are not exclusive on
-    # the right side.
-    selected_bin = numpy.searchsorted(thresholds, x, side="right") - 1
-
-    # Calc last threshold for each individual
-    threshold = thresholds[selected_bin]
-
-    # Increment for each individual in the corresponding interval.
-    increment_to_calc = x - threshold
-
-    # If each individual has its own rates or the rates are scaled, we can't use the
-    # intercept, which was generated in the parameter loading.
-    if rates_multiplier is not None:
-        # Initialize Series containing 0 for all individuals.
-        out = intercepts_at_lower_thresholds[0]
-
-        # Go through all intervals except the first and last.
-        for i in range(2, num_intervals):
-            threshold_incr = thresholds[i] - thresholds[i - 1]
-            for pol in range(1, degree_polynomial + 1):
-                # We only calculate the intercepts for individuals who are in this or
-                # higher interval. Hence we have to use the individual rates.
-                if selected_bin >= i:
-                    out += (
-                        rates_multiplier * rates[pol - 1, i - 1] * threshold_incr**pol
-                    )
-    # If rates remain the same, everything is a lot easier.
-    else:
-        # We assign each individual the pre-calculated intercept.
-        out = intercepts_at_lower_thresholds[selected_bin]
-    # Intialize a multiplyer for 1 if it is not given.
-    rates_multiplier = 1 if rates_multiplier is None else rates_multiplier
-
-    if selected_bin > 0:
-        # Now add the evaluation of the increment
-        for pol in range(1, degree_polynomial + 1):
-            out += (
-                rates[pol - 1][selected_bin]
-                * rates_multiplier
-                * (increment_to_calc**pol)
-            )
-
-    return out
+    numpy.testing.assert_allclose(numpy.array(actual), expected, atol=0.01)
