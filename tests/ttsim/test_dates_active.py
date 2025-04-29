345d6c6cdc26fd96a3e634d7cda7be2000f87dcd
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-04-29 10:12:00 +0200

    Add tests.

--- tests/ttsim/test_dates_active.py
+++ tests/ttsim/test_dates_active.py
@@ -134,6 +134,7 @@ def identity(x):
 @pytest.mark.parametrize(
     "orig_ttsim_objects_tree",
     [
+        # Same global module, no overlapping periods.
         {
             ("a",): policy_function(
                 start_date="2023-01-01",
@@ -146,6 +147,7 @@ def identity(x):
                 leaf_name="f",
             )(identity),
         },
+        # Same submodule, no overlapping periods.
         {
             ("c", "a"): policy_function(
                 start_date="2023-01-01",
@@ -153,6 +155,30 @@ def identity(x):
                 leaf_name="f",
             )(identity),
             ("c", "b"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-02-28",
+                leaf_name="g",
+            )(identity),
+        },
+        # Different modules, no overlapping periods.
+        {
+            ("c", "f"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+            )(identity),
+            ("d", "f"): policy_function(
+                start_date="2023-02-01",
+                end_date="2023-02-28",
+            )(identity),
+        },
+        # Different paths, overlapping periods.
+        {
+            ("x", "c", "a"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+            ("y", "c", "b"): policy_function(
                 start_date="2023-01-01",
                 end_date="2023-02-28",
                 leaf_name="g",
@@ -169,6 +195,7 @@ def test_dates_active_no_conflicts(orig_ttsim_objects_tree):
 @pytest.mark.parametrize(
     "orig_ttsim_objects_tree",
     [
+        # Exact overlap.
         {
             ("a",): policy_function(
                 start_date="2023-01-01",
@@ -181,18 +208,20 @@ def test_dates_active_no_conflicts(orig_ttsim_objects_tree):
                 leaf_name="f",
             )(identity),
         },
+        # Active period for "a" is subset of "b".
         {
-            ("c", "a"): policy_function(
+            ("a"): policy_function(
                 start_date="2023-01-01",
                 end_date="2023-01-31",
                 leaf_name="f",
             )(identity),
-            ("c", "b"): policy_function(
+            ("b"): policy_function(
                 start_date="2021-01-02",
                 end_date="2023-02-01",
                 leaf_name="f",
             )(identity),
         },
+        # Some overlap.
         {
             ("a",): policy_function(
                 start_date="2023-01-02",
@@ -203,6 +232,30 @@ def test_dates_active_no_conflicts(orig_ttsim_objects_tree):
                 start_date="2022-01-01",
                 end_date="2023-01-31",
                 leaf_name="f",
+            )(identity),
+        },
+        # Same as before, but defined in different modules.
+        {
+            ("c", "a"): policy_function(
+                start_date="2023-01-02",
+                end_date="2023-02-01",
+                leaf_name="f",
+            )(identity),
+            ("d", "b"): policy_function(
+                start_date="2022-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+        },
+        # Same as before, but defined in different modules without leaf name.
+        {
+            ("c", "f"): policy_function(
+                start_date="2023-01-02",
+                end_date="2023-02-01",
+            )(identity),
+            ("d", "f"): policy_function(
+                start_date="2022-01-01",
+                end_date="2023-01-31",
             )(identity),
         },
     ],
