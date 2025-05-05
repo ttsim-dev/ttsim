33a6a4152f3bacd885ae9e3c6dea7c56368f5b63
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-05-05 07:33:30 +0200

    Integrate tests of start_date / end_date in test_policy_environment.

--- tests/ttsim/test_policy_environment.py
+++ tests/ttsim/test_policy_environment.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import date, timedelta
+import datetime
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -19,13 +19,15 @@ from ttsim import (
     set_up_policy_environment,
 )
 from ttsim.policy_environment import (
+    ConflictingTimeDependentObjectsError,
     _fail_if_name_of_last_branch_element_not_leaf_name_of_function,
     _load_parameter_group_from_yaml,
     active_ttsim_objects_tree,
+    fail_because_of_clashes,
 )
 
 if TYPE_CHECKING:
-    from ttsim.typing import NestedTTSIMObjectDict
+    from ttsim.typing import FlatTTSIMObjectDict, NestedTTSIMObjectDict
 
 YAML_PATH = Path(__file__).parent / "test_parameters"
 
@@ -138,7 +140,7 @@ def test_access_different_date_jahresanfang():
     [
         (
             {"housing_benefits": {"eligibility": {"requirement_fulfilled_fam": None}}},
-            date(2019, 12, 31),
+            datetime.date(2019, 12, 31),
             "requirement_fulfilled_fam_not_considering_children",
             "requirement_fulfilled_fam_considering_children",
         ),
@@ -146,13 +148,13 @@ def test_access_different_date_jahresanfang():
 )
 def test_load_functions_tree_for_date(
     tree: NestedTTSIMObjectDict,
-    last_day: date,
+    last_day: datetime.date,
     function_name_last_day: str,
     function_name_next_day: str,
 ):
     functions_last_day = active_ttsim_objects_tree(root=METTSIM_ROOT, date=last_day)
     functions_next_day = active_ttsim_objects_tree(
-        root=METTSIM_ROOT, date=last_day + timedelta(days=1)
+        root=METTSIM_ROOT, date=last_day + datetime.timedelta(days=1)
     )
 
     accessor = optree.tree_accessors(tree, none_is_leaf=True)[0]
@@ -204,3 +206,257 @@ def test_input_is_recognized_as_potential_group_id():
 def test_p_id_not_recognized_as_potential_group_id():
     environment = set_up_policy_environment(root=METTSIM_ROOT, date="2020-01-01")
     assert "p" not in environment.grouping_levels
+
+
+@pytest.mark.parametrize(
+    "date_string, expected",
+    [
+        ("2023-01-20", datetime.date(2023, 1, 20)),
+    ],
+)
+def test_start_date_valid(date_string: str, expected: datetime.date):
+    @policy_function(start_date=date_string)
+    def test_func():
+        pass
+
+    assert test_func.start_date == expected
+
+
+@pytest.mark.parametrize(
+    "date_string",
+    [
+        "20230120",
+        "20.1.2023",
+        "20th January 2023",
+    ],
+)
+def test_start_date_invalid(date_string: str):
+    with pytest.raises(ValueError):
+
+        @policy_function(start_date=date_string)
+        def test_func():
+            pass
+
+
+def test_start_date_missing():
+    @policy_function()
+    def test_func():
+        pass
+
+    assert test_func.start_date == datetime.date(1900, 1, 1)
+
+
+# End date -------------------------------------------------
+
+
+@pytest.mark.parametrize(
+    "date_string, expected",
+    [
+        ("2023-01-20", datetime.date(2023, 1, 20)),
+    ],
+)
+def test_end_date_valid(date_string: str, expected: datetime.date):
+    @policy_function(end_date=date_string)
+    def test_func():
+        pass
+
+    assert test_func.end_date == expected
+
+
+@pytest.mark.parametrize(
+    "date_string",
+    [
+        "20230120",
+        "20.1.2023",
+        "20th January 2023",
+    ],
+)
+def test_end_date_invalid(date_string: str):
+    with pytest.raises(ValueError):
+
+        @policy_function(end_date=date_string)
+        def test_func():
+            pass
+
+
+def test_end_date_missing():
+    @policy_function()
+    def test_func():
+        pass
+
+    assert test_func.end_date == datetime.date(2100, 12, 31)
+
+
+# Change name ----------------------------------------------
+
+
+def test_dates_active_change_name_given():
+    @policy_function(leaf_name="renamed_func")
+    def test_func():
+        pass
+
+    assert test_func.leaf_name == "renamed_func"
+
+
+def test_dates_active_change_name_missing():
+    @policy_function()
+    def test_func():
+        pass
+
+    assert test_func.leaf_name == "test_func"
+
+
+# Empty interval -------------------------------------------
+
+
+def test_dates_active_empty_interval():
+    with pytest.raises(ValueError):
+
+        @policy_function(start_date="2023-01-20", end_date="2023-01-19")
+        def test_func():
+            pass
+
+
+# Conflicts ------------------------------------------------
+
+
+def identity(x):
+    return x
+
+
+@pytest.mark.parametrize(
+    "orig_ttsim_objects_tree",
+    [
+        # Same global module, no overlapping periods.
+        {
+            ("a",): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+            ("b",): policy_function(
+                start_date="2023-02-01",
+                end_date="2023-02-28",
+                leaf_name="f",
+            )(identity),
+        },
+        # Same submodule, no overlapping periods.
+        {
+            ("c", "a"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+            ("c", "b"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-02-28",
+                leaf_name="g",
+            )(identity),
+        },
+        # Different modules, no overlapping periods.
+        {
+            ("c", "f"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+            )(identity),
+            ("d", "f"): policy_function(
+                start_date="2023-02-01",
+                end_date="2023-02-28",
+            )(identity),
+        },
+        # Different paths, overlapping periods.
+        {
+            ("x", "c", "a"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+            ("y", "c", "b"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-02-28",
+                leaf_name="g",
+            )(identity),
+        },
+    ],
+)
+def test_dates_active_no_conflicts(orig_ttsim_objects_tree):
+    fail_because_of_clashes(
+        orig_ttsim_objects_tree=orig_ttsim_objects_tree,
+        orig_yaml_tree={},
+    )
+
+
+@pytest.mark.parametrize(
+    "orig_ttsim_objects_tree",
+    [
+        # Exact overlap.
+        {
+            ("a",): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+            ("b",): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+        },
+        # Active period for "a" is subset of "b".
+        {
+            ("a"): policy_function(
+                start_date="2023-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+            ("b"): policy_function(
+                start_date="2021-01-02",
+                end_date="2023-02-01",
+                leaf_name="f",
+            )(identity),
+        },
+        # Some overlap.
+        {
+            ("a",): policy_function(
+                start_date="2023-01-02",
+                end_date="2023-02-01",
+                leaf_name="f",
+            )(identity),
+            ("b",): policy_function(
+                start_date="2022-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+        },
+        # Same as before, but defined in different modules.
+        {
+            ("c", "a"): policy_function(
+                start_date="2023-01-02",
+                end_date="2023-02-01",
+                leaf_name="f",
+            )(identity),
+            ("d", "b"): policy_function(
+                start_date="2022-01-01",
+                end_date="2023-01-31",
+                leaf_name="f",
+            )(identity),
+        },
+        # Same as before, but defined in different modules without leaf name.
+        {
+            ("c", "f"): policy_function(
+                start_date="2023-01-02",
+                end_date="2023-02-01",
+            )(identity),
+            ("d", "f"): policy_function(
+                start_date="2022-01-01",
+                end_date="2023-01-31",
+            )(identity),
+        },
+    ],
+)
+def test_dates_active_with_conflicts(orig_ttsim_objects_tree: FlatTTSIMObjectDict):
+    with pytest.raises(ConflictingTimeDependentObjectsError):
+        fail_because_of_clashes(
+            orig_ttsim_objects_tree=orig_ttsim_objects_tree,
+            orig_yaml_tree={},
+        )
