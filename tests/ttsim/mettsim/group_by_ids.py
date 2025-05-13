bf47bb7e487d07c6a1843a92db5998751651ac24
mj023 <max.jahn45@gmail.com>
2025-05-13 18:28:09 +0200

    Make METTSIM group creation jittable

--- tests/ttsim/mettsim/group_by_ids.py
+++ tests/ttsim/mettsim/group_by_ids.py
@@ -1,6 +1,6 @@
 from ttsim import group_creation_function
 from ttsim.config import numpy_or_jax as np
-
+import jax
 
 @group_creation_function()
 def sp_id(
@@ -10,26 +10,15 @@ def sp_id(
     """
     Compute the spouse (sp) group ID for each person.
     """
-    p_id_to_sp_id: dict[int, int] = {}
-    next_sp_id: int = 0
-    result: list[int] = []
+    n = 1000000
+    p_id_spouse = np.where(p_id_spouse < 0, p_id, p_id_spouse)
+    result = np.maximum(p_id, p_id_spouse) + np.minimum(p_id, p_id_spouse) * n
 
-    for index, current_p_id in enumerate(map(int, p_id)):
-        current_p_id_spouse = int(p_id_spouse[index])
-
-        if current_p_id_spouse >= 0 and current_p_id_spouse in p_id_to_sp_id:
-            result.append(p_id_to_sp_id[current_p_id_spouse])
-            continue
-
-        # New married couple
-        result.append(next_sp_id)
-        p_id_to_sp_id[current_p_id] = next_sp_id
-        next_sp_id += 1
-
-    return np.array(result)
+    return result
 
 
 @group_creation_function()
+@jax.jit
 def fam_id(
     p_id_spouse: np.ndarray,
     p_id: np.ndarray,
@@ -40,53 +29,22 @@ def fam_id(
     """
     Compute the family ID for each person.
     """
-    # Build indexes
-    p_id_to_index: dict[int, int] = {}
-    p_id_to_p_ids_children: dict[int, list[int]] = {}
+    n = 1000000
+    children = np.isin(p_id, p_id_parent_1) + np.isin(p_id, p_id_parent_2)
+    fg_id = np.where(
+        p_id_spouse < 0,
+        p_id + p_id * n,
+        np.maximum(p_id, p_id_spouse) + np.minimum(p_id, p_id_spouse) * n,
+    )
+    fg_id = np.where(
+        (p_id_parent_1 >= 0) * (age < 25) * (1 - children),
+        fg_id[p_id_parent_1],
+        fg_id,
+    )
+    fg_id = np.where(
+        (p_id_parent_2 >= 0) * (age < 25) * (1 - children),
+        fg_id[p_id_parent_2],
+        fg_id,
+    )
 
-    for index, current_p_id in enumerate(map(int, p_id)):
-        p_id_to_index[current_p_id] = index
-        current_p_id_parent_1 = int(p_id_parent_1[index])
-        current_p_id_parent_2 = int(p_id_parent_2[index])
-
-        if current_p_id_parent_1 >= 0:
-            if current_p_id_parent_1 not in p_id_to_p_ids_children:
-                p_id_to_p_ids_children[current_p_id_parent_1] = []
-            p_id_to_p_ids_children[current_p_id_parent_1].append(current_p_id)
-
-        if current_p_id_parent_2 >= 0:
-            if current_p_id_parent_2 not in p_id_to_p_ids_children:
-                p_id_to_p_ids_children[current_p_id_parent_2] = []
-            p_id_to_p_ids_children[current_p_id_parent_2].append(current_p_id)
-
-    p_id_to_fam_id = {}
-    next_fam_id = 0
-
-    for index, current_p_id in enumerate(map(int, p_id)):
-        # Already assigned a fam_id to this p_id via spouse / parent
-        if current_p_id in p_id_to_fam_id:
-            continue
-
-        p_id_to_fam_id[current_p_id] = next_fam_id
-
-        current_p_id_spouse = int(p_id_spouse[index])
-        current_p_id_children = p_id_to_p_ids_children.get(current_p_id, [])
-
-        # Assign fam_id to spouse
-        if current_p_id_spouse >= 0:
-            p_id_to_fam_id[current_p_id_spouse] = next_fam_id
-
-        # Assign fam_id to children
-        for current_p_id_child in current_p_id_children:
-            child_index = p_id_to_index[current_p_id_child]
-            child_age = age[child_index]
-            child_p_id_children = p_id_to_p_ids_children.get(current_p_id_child, [])
-
-            if child_age < 25 and len(child_p_id_children) == 0:
-                p_id_to_fam_id[current_p_id_child] = next_fam_id
-
-        next_fam_id += 1
-
-    # Compute result vector
-    result = [p_id_to_fam_id[current_p_id] for current_p_id in map(int, p_id)]
-    return np.array(result)
+    return fg_id
