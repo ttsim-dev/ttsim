0b9b259e5089cadb982b6778654fc236d0c27352
Marvin Immesberger <immesberger@posteo.de>
2025-05-16 23:20:53 +0200

    Forogt to add type hints in commit.

--- src/_gettsim/sozialversicherung/midijob.py
+++ src/_gettsim/sozialversicherung/midijob.py
@@ -92,7 +92,9 @@ def midijob_faktor_f_mit_minijob_steuerpauschale_ab_2005_bis_2022_09(
     sozialversicherung__kranken__beitrag__beitragssatz_arbeitgeber_jahresanfang: float,
     sozialversicherung__rente__beitrag__parameter_beitragssatz_jahresanfang: float,
     sozialversicherung__arbeitslosen__beitrag__parameter_beitragssatz_jahresanfang: float,
-    sozialversicherung__pflege__beitrag__beitragssatz_abhängig_von_anzahl_kinder_jahresanfang: dict,
+    sozialversicherung__pflege__beitrag__beitragssatz_abhängig_von_anzahl_kinder_jahresanfang: dict[
+        str, float
+    ],
     geringfügige_einkommen_params: dict,
     sozialversicherung__kranken__beitrag__arbeitgeberpauschale_bei_geringfügiger_beschäftigung: float,
     sozialversicherung__rente__beitrag__arbeitgeberpauschale_bei_geringfügiger_beschäftigung: float,
