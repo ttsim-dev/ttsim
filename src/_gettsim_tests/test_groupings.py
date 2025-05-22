c7d5cf536b35a0dec0afe8b871fa7c9d54b1d4a0
Max Jahn <max.jahn45@gmail.com>
2025-05-22 22:38:43 +0200

    Make ID creation functions jittable (#905)
    ### What problem do you want to solve?
    
    Make the ID creation functions in Gettsim and Mettsim jittable.
    
    
    ### Todo
    
    - [x] Make Gettsim ID's jittable
    - [x] Make METTSIM ID's jittable.
    - [x] Change tests to the new ID values
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hans-Martin von Gaudecker <hmgaudecker@gmail.com>

--- src/_gettsim_tests/test_groupings.py
+++ src/_gettsim_tests/test_groupings.py
@@ -10,7 +10,6 @@ from _gettsim_tests.utils import (
     load_policy_test_data,
 )
 from ttsim import compute_taxes_and_transfers
-from ttsim.config import numpy_or_jax as np
 
 test_data = load_policy_test_data("groupings")
 
@@ -32,27 +31,3 @@ def test_groupings(test: PolicyTest):
         flat_result.values(), flat_expected_output_tree.values()
     ):
         assert_array_almost_equal(result, expected, decimal=2)
-
-
-def test_fail_to_compute_sn_id_if_married_but_gemeinsam_veranlagt_differs():
-    data = {
-        "p_id": np.array([0, 1]),
-        "familie": {
-            "p_id_ehepartner": np.array([1, 0]),
-        },
-        "einkommensteuer": {
-            "gemeinsam_veranlagt": np.array([False, True]),
-        },
-    }
-
-    environment = cached_set_up_policy_environment("2023-01-01")
-
-    with pytest.raises(
-        ValueError,
-        match="have different values for gemeinsam_veranlagt",
-    ):
-        compute_taxes_and_transfers(
-            data_tree=data,
-            environment=environment,
-            targets_tree={"sn_id": None},
-        )
