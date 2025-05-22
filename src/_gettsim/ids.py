df6421d8d67cadb33fd46023275f05e2f9b9707b
mj023 <max.jahn45@gmail.com>
2025-05-22 18:55:44 +0200

    Revert Tests, Add comments

--- src/_gettsim/ids.py
+++ src/_gettsim/ids.py
@@ -50,42 +50,35 @@ def fg_id(
     """
     n = np.max(p_id) + 1
 
-    familie__p_id_elternteil_1_loc = familie__p_id_elternteil_1
-    familie__p_id_elternteil_2_loc = familie__p_id_elternteil_2
+    # Get the array index for all p_ids of parents
+    p_id_elternteil_1_loc = familie__p_id_elternteil_1
+    p_id_elternteil_2_loc = familie__p_id_elternteil_2
     for i in range(p_id.shape[0]):
-        familie__p_id_elternteil_1_loc = np.where(
-            familie__p_id_elternteil_1 == p_id[i], i, familie__p_id_elternteil_1_loc
+        p_id_elternteil_1_loc = np.where(
+            familie__p_id_elternteil_1 == p_id[i], i, p_id_elternteil_1_loc
         )
-        familie__p_id_elternteil_2_loc = np.where(
-            familie__p_id_elternteil_2 == p_id[i], i, familie__p_id_elternteil_2_loc
+        p_id_elternteil_2_loc = np.where(
+            familie__p_id_elternteil_2 == p_id[i], i, p_id_elternteil_2_loc
         )
 
     children = np.isin(p_id, familie__p_id_elternteil_1) + np.isin(
         p_id, familie__p_id_elternteil_2
     )
+
+    # Assign the same fg_id to everybody who has an Einstandspartner,
+    # otherwise create a new one from p_id
     fg_id = np.where(
         arbeitslosengeld_2__p_id_einstandspartner < 0,
         p_id + p_id * n,
         np.maximum(p_id, arbeitslosengeld_2__p_id_einstandspartner)
         + np.minimum(p_id, arbeitslosengeld_2__p_id_einstandspartner) * n,
     )
-    fg_id = np.where(
-        (familie__p_id_elternteil_1_loc >= 0)
-        * (fg_id == p_id + p_id * n)
-        * (hh_id == hh_id[familie__p_id_elternteil_1_loc])
-        * (alter < 25)
-        * (1 - children),
-        fg_id[familie__p_id_elternteil_1_loc],
-        fg_id,
+
+    fg_id = assign_parents_fg_id(
+        fg_id, p_id, p_id_elternteil_1_loc, hh_id, alter, children, n
     )
-    fg_id = np.where(
-        (familie__p_id_elternteil_2_loc >= 0)
-        * (fg_id == p_id + p_id * n)
-        * (hh_id == hh_id[familie__p_id_elternteil_2_loc])
-        * (alter < 25)
-        * (1 - children),
-        fg_id[familie__p_id_elternteil_2_loc],
-        fg_id,
+    fg_id = assign_parents_fg_id(
+        fg_id, p_id, p_id_elternteil_2_loc, hh_id, alter, children, n
     )
 
     return reorder_ids(fg_id)
@@ -110,8 +103,9 @@ def bg_id(
     # TODO(@MImmesberger): Remove hard-coded number
     # https://github.com/iza-institute-of-labor-economics/gettsim/issues/668
     offset = np.max(fg_id) + 1
+    # Create new id for everyone who is not part of the Bedarfsgemeinschaft
     bg_id = np.where(
-        np.logical_and(arbeitslosengeld_2__eigenbedarf_gedeckt, alter < 25),
+        (arbeitslosengeld_2__eigenbedarf_gedeckt) * (alter < 25),
         offset + p_id,
         fg_id,
     )
@@ -129,14 +123,14 @@ def eg_id(
     A couple whose members are deemed to be responsible for each other.
     """
     n = np.max(p_id) + 1
-    arbeitslosengeld_2__p_id_einstandspartner = np.where(
+    p_id_einstandspartner__or_own_p_id = np.where(
         arbeitslosengeld_2__p_id_einstandspartner < 0,
         p_id,
         arbeitslosengeld_2__p_id_einstandspartner,
     )
     result = (
-        np.maximum(p_id, arbeitslosengeld_2__p_id_einstandspartner)
-        + np.minimum(p_id, arbeitslosengeld_2__p_id_einstandspartner) * n
+        np.maximum(p_id, p_id_einstandspartner__or_own_p_id)
+        + np.minimum(p_id, p_id_einstandspartner__or_own_p_id) * n
     )
 
     return reorder_ids(result)
@@ -175,13 +169,13 @@ def sn_id(
     """
 
     n = np.max(p_id) + 1
+
     p_id_ehepartner_or_own_p_id = np.where(
-        np.logical_and(
-            familie__p_id_ehepartner >= 0, einkommensteuer__gemeinsam_veranlagt
-        ),
+        (familie__p_id_ehepartner >= 0) * (einkommensteuer__gemeinsam_veranlagt),
         familie__p_id_ehepartner,
         p_id,
     )
+
     result = (
         np.maximum(p_id, p_id_ehepartner_or_own_p_id)
         + np.minimum(p_id, p_id_ehepartner_or_own_p_id) * n
@@ -195,6 +189,41 @@ def reorder_ids(ids: np.ndarray) -> np.ndarray:
     sorting = np.argsort(ids)
     ids_sorted = ids[sorting]
     index_after_sort = np.arange(ids.shape[0])[sorting]
+    # Look for difference from previous entry in sorted array
     diff_to_prev = np.where(np.diff(ids_sorted) >= 1, 1, 0)
+    # Sum up all differences to get new id
     cons_ids = np.concatenate((np.asarray([0]), np.cumsum(diff_to_prev)))
     return cons_ids[np.argsort(index_after_sort)]
+
+
+def assign_parents_fg_id(
+    fg_id: np.ndarray,
+    p_id: np.ndarray,
+    p_id_elternteil_loc: np.ndarray,
+    hh_id: np.ndarray,
+    alter: np.ndarray,
+    children: np.ndarray,
+    n: np.ndarray,
+) -> np.ndarray:
+    """Get the fg_id of the childs parents.
+
+    If the child is not married, has no children, is under 25 and in the same household,
+    assign the fg_id of its parents."""
+
+    # TODO(@MImmesberger): Remove input variable eigenbedarf_gedeckt
+    # once Bedarfsgemeinschaften are fully endogenous
+    # https://github.com/iza-institute-of-labor-economics/gettsim/issues/763
+    # TODO(@MImmesberger): Remove hard-coded number
+    # https://github.com/iza-institute-of-labor-economics/gettsim/issues/668
+
+    new_ids = np.where(
+        (p_id_elternteil_loc >= 0)
+        * (fg_id == p_id + p_id * n)
+        * (hh_id == hh_id[p_id_elternteil_loc])
+        * (alter < 25)
+        * (1 - children),
+        fg_id[p_id_elternteil_loc],
+        fg_id,
+    )
+
+    return new_ids
