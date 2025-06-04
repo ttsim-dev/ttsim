ec4498022ba3b74b487953710b091a89f7a249d0
Marvin Immesberger <immesberger@posteo.de>
2025-06-04 14:05:29 +0200

    Remove superseded test.

--- tests/ttsim/test_mettsim.py
+++ tests/ttsim/test_mettsim.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-import datetime
 from pathlib import Path
 
-import numpy as np
 import pytest
 from mettsim.config import METTSIM_ROOT
 
@@ -29,21 +27,3 @@ def test_mettsim(test: PolicyTest):
         execute_test(test, root=METTSIM_ROOT, jit=True)
     else:
         execute_test(test, root=METTSIM_ROOT, jit=False)
-
-
-@pytest.mark.xfail(
-    reason="Testing infrastructure cannot handle single-element expected output"
-)
-def test_mettsim_single_element_expected_output():
-    test = PolicyTest(
-        info={"precision_atol": 0.01},
-        input_tree={
-            "p_id": np.array([0]),
-            "property_tax": {"acre_size_in_hectares": np.array([20])},
-        },
-        expected_output_tree={"property_tax": {"amount_y": np.array([1000.0])}},
-        path=None,
-        date=datetime.date(2020, 1, 1),
-        test_dir=TEST_DIR,
-    )
-    execute_test(test, root=METTSIM_ROOT, jit=False)
