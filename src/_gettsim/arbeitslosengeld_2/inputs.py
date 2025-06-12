f57efa1de0e9eda07655d1dc193ae32399478cd9
Marvin Immesberger <immesberger@posteo.de>
2025-06-12 14:31:37 +0200

    Improve docstring for eigenbedarf_gedeckt.

--- src/_gettsim/arbeitslosengeld_2/inputs.py
+++ src/_gettsim/arbeitslosengeld_2/inputs.py
@@ -15,7 +15,7 @@ def bezug_im_vorjahr() -> bool:
 # https://github.com/iza-institute-of-labor-economics/gettsim/issues/763
 @policy_input(start_date="2005-01-01")
 def eigenbedarf_gedeckt() -> bool:
-    """Received Arbeitslosengeld II / Bürgergeld in previous year."""
+    """Needs according to SGB II are covered by own income."""
 
 
 @policy_input(start_date="2005-01-01", foreign_key_type=FKType.MUST_NOT_POINT_TO_SELF)
