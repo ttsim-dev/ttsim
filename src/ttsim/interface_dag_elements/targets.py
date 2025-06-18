a931fb09bb6f0d2efbccbd53ce5c18ed0464ab07
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-06-18 10:38:08 +0200

    Pick targets as the PolicyFunctions in the policy_environment should they not be provided.

--- src/ttsim/interface_dag_elements/targets.py
+++ src/ttsim/interface_dag_elements/targets.py
@@ -6,23 +6,34 @@ import dags.tree as dt
 
 from ttsim.interface_dag_elements.interface_node_objects import (
     interface_function,
-    interface_input,
 )
+from ttsim.tt_dag_elements.column_objects_param_function import ColumnFunction
 
 if TYPE_CHECKING:
     from ttsim.interface_dag_elements.typing import (
+        NestedPolicyEnvironment,
         NestedTargetDict,
         OrderedQNames,
     )
 
 
-@interface_input()
-def tree() -> NestedTargetDict:
-    """Targets as a tree.
+@interface_function()
+def tree(policy_environment: NestedPolicyEnvironment) -> NestedTargetDict:
+    """Targets as a tree. Will typically be provided by the user.
 
     If requesting `df_with_mapper` as the targets, the leaves must be the desired
     column names.
+
+    If not provided, the targets will be inferred from the policy environment by using
+    all ColumnFunctions in the policy environment.
     """
+    return dt.unflatten_from_tree_paths(
+        {
+            k: None
+            for k, v in dt.flatten_to_tree_paths(policy_environment).items()
+            if isinstance(v, ColumnFunction)
+        }
+    )
 
 
 @interface_function()
