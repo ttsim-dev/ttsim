fdd26e7f038f7405d3d796fb502b8a1694b2b83f
Marvin Immesberger <74215010+MImmesberger@users.noreply.github.com>
2025-06-20 12:52:31 +0200

    InputDependentInterfaceFunction (#977)
    DAG depends on input data. Allows to specify input data in different
    ways (nested df, df with mapper, tree).
    
    Supersedes #975.
    
    Todo:
    - [x] Testing (other than end-to-end)
    
    ---------
    
    Co-authored-by: Hans-Martin von Gaudecker <hmgaudecker@gmail.com>

--- tests/ttsim/test_interface_dag.py
+++ tests/ttsim/test_interface_dag.py
@@ -14,7 +14,11 @@ from ttsim.interface_dag import (
 )
 from ttsim.interface_dag_elements import InterfaceDAGElements
 from ttsim.interface_dag_elements.fail_if import format_list_linewise
-from ttsim.interface_dag_elements.interface_node_objects import fail_or_warn_function
+from ttsim.interface_dag_elements.interface_node_objects import (
+    InterfaceFunctionVariant,
+    fail_or_warn_function,
+    input_dependent_interface_function,
+)
 from ttsim.plot_dag import dummy_callable
 from ttsim.tt_dag_elements.column_objects_param_function import policy_function
 
@@ -24,6 +28,38 @@ from ttsim.tt_dag_elements.column_objects_param_function import policy_function
     include_if_any_element_present=["b"],
 )
 def some_fail_or_warn_function() -> None:
+    pass
+
+
+@input_dependent_interface_function(
+    variants=[
+        InterfaceFunctionVariant(
+            required_input_qnames=["input_1"],
+            function=lambda input_1: input_1,
+        ),
+        InterfaceFunctionVariant(
+            required_input_qnames=["input_2", "n1__input_2"],
+            function=lambda input_2, n1__input_2: input_2 + n1__input_2,
+        ),
+    ]
+)
+def some_input_dependent_interface_function() -> int:
+    pass
+
+
+@input_dependent_interface_function(
+    variants=[
+        InterfaceFunctionVariant(
+            required_input_qnames=["input_1"],
+            function=lambda input_1: input_1,
+        ),
+        InterfaceFunctionVariant(
+            required_input_qnames=["input_1", "n1__input_2"],
+            function=lambda input_1, n1__input_2: input_1 + n1__input_2,
+        ),
+    ]
+)
+def some_input_dependent_interface_function_with_conflicting_variants() -> int:
     pass
 
 
@@ -162,3 +198,40 @@ def test_harmonize_inputs_tree_input():
         "backend": "numpy",
         "rounding": True,
     }
+
+
+@pytest.mark.parametrize(
+    (
+        "input_qnames",
+        "expected_function_args",
+    ),
+    [
+        (
+            ["input_2", "n1__input_2"],
+            ["input_2", "n1__input_2"],
+        ),
+        (
+            ["input_1"],
+            ["input_1"],
+        ),
+        (
+            ["input_1", "n2__input_2"],
+            ["input_1"],
+        ),
+    ],
+)
+def test_input_dependent_interface_functions_have_correct_args(
+    input_qnames, expected_function_args
+):
+    func = some_input_dependent_interface_function.resolve_to_static_interface_function(
+        input_qnames
+    )
+    assert list(inspect.signature(func).parameters.keys()) == expected_function_args
+
+
+def test_input_dependent_interface_functions_with_conflicting_variants():
+    match = r"Multiple sets of inputs were found that satisfy the requirements:"
+    with pytest.raises(ValueError, match=match):
+        some_input_dependent_interface_function_with_conflicting_variants.resolve_to_static_interface_function(
+            ["input_1", "n1__input_2"]
+        )
