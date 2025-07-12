1fa476a8bd4b09dac5969680978de527bd136d37
Marvin Immesberger <74215010+MImmesberger@users.noreply.github.com>
2025-07-12 14:11:41 +0200

    Update src/_gettsim/sozialversicherung/pflege/beitrag/inputs.py
    Co-authored-by: Hans-Martin von Gaudecker <hmgaudecker@gmail.com>

--- src/_gettsim/sozialversicherung/pflege/beitrag/inputs.py
+++ src/_gettsim/sozialversicherung/pflege/beitrag/inputs.py
@@ -7,5 +7,5 @@ from ttsim.tt_dag_elements import policy_input
 
 @policy_input()
 def hat_kinder() -> bool:
-    """Parent of at least one child (including children in different households,
+    """Parent of at least one child (including children in other households,
     adopted, adult, and deceased children)."""
