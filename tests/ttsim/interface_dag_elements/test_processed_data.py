7e1dbda426612cd22888ffff99cec8f233296803
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-07-20 15:21:09 +0200

    Add another test case.

--- tests/ttsim/interface_dag_elements/test_processed_data.py
+++ tests/ttsim/interface_dag_elements/test_processed_data.py
@@ -29,8 +29,8 @@ def test_processed_data(input_data__flat, xnp):
 
 
 def test_processed_data_foreign_key_out_of_bounds(xnp):
-    # Add invalid numbers (-5, too large), in foreign key. Should be unchanged, error
-    # will be raised in `fail_if.foreign_keys_are_invalid_in_data` if the
+    # Add out-of-bounds numbers (-5, 999), in foreign key. Should be unchanged, error
+    # will be raised in `fail_if.foreign_keys_are_invalid_in_data`.
     input_data__flat = {
         ("p_id",): numpy.array([5, 333, 7, 2]),
         ("hh_id",): numpy.array([55555, 7, 3, 55555]),
@@ -39,7 +39,26 @@ def test_processed_data_foreign_key_out_of_bounds(xnp):
     expected = {
         "p_id": xnp.array([1, 3, 2, 0]),
         "hh_id": xnp.array([2, 1, 0, 2]),
-        "n0__p_id_whatever": xnp.array([-1, 3, -5, 999]),  # 999 preserved unchanged
+        "n0__p_id_whatever": xnp.array([-1, 3, -5, 999]),  # -5, 999 preserved unchanged
+    }
+    pd.testing.assert_frame_equal(
+        pd.DataFrame(processed_data(input_data__flat=input_data__flat, xnp=xnp)),
+        pd.DataFrame(expected),
+    )
+
+
+def test_processed_data_foreign_key_inside_bounds(xnp):
+    # Add non-existent foreign key (22). Should be unchanged, error will be raised in
+    # `fail_if.foreign_keys_are_invalid_in_data`.
+    input_data__flat = {
+        ("p_id",): numpy.array([5, 333, 7, 2]),
+        ("hh_id",): numpy.array([55555, 7, 3, 55555]),
+        ("n0", "p_id_whatever"): numpy.array([-1, 333, 22, -1]),
+    }
+    expected = {
+        "p_id": xnp.array([1, 3, 2, 0]),
+        "hh_id": xnp.array([2, 1, 0, 2]),
+        "n0__p_id_whatever": xnp.array([-1, 3, 2, -1]),
     }
     pd.testing.assert_frame_equal(
         pd.DataFrame(processed_data(input_data__flat=input_data__flat, xnp=xnp)),
