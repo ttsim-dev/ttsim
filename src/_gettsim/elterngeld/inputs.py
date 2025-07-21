848c774ebf4f42dc1a43205a741d97bfa0995649
Marvin Immesberger <immesberger@posteo.de>
2025-07-21 18:09:51 +0200

    Elterngeld.

--- src/_gettsim/elterngeld/inputs.py
+++ src/_gettsim/elterngeld/inputs.py
@@ -16,10 +16,23 @@ def claimed() -> bool:
 
 
 @policy_input()
-def nettoeinkommen_vorjahr_m() -> float:
-    """Net wage in the 12 months before birth of youngest child."""
+def mean_nettoeinkommen_in_12_monaten_vor_geburt_m() -> float:
+    """Mean net wage in the 12 months before birth of youngest child.
+
+    To compute this value using GETTSIM:
+    1. Use `elterngeld__mean_nettoeinkommen_für_bemessungsgrundllage_nach_geburt_m` as
+    the TT target
+    2. Apply it to data from the last 12 months before the birth of the youngest child
+    3. Use the result as input for this column.
+    """
 
 
 @policy_input()
 def zu_versteuerndes_einkommen_vorjahr_y_sn() -> float:
-    """Taxable income in the calendar year prior to the youngest child's birth year."""
+    """Taxable income in the calendar year prior to the youngest child's birth year.
+
+    To compute this value using GETTSIM:
+    1. Use `einkommensteuer__zu_versteuerndes_einkommen_y_sn` as the TT target
+    2. Apply it to data from the year prior to the youngest child's birth year
+    3. Use the result as input for this column.
+    """
