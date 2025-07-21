d83ebed05950d7f64f5d45416419453864bca9f5
Marvin Immesberger <immesberger@posteo.de>
2025-07-21 10:30:41 +0200

    Clarify meaning of input variable via docstring.

--- src/_gettsim/einkommensteuer/einkünfte/inputs.py
+++ src/_gettsim/einkommensteuer/einkünfte/inputs.py
@@ -7,4 +7,9 @@ from ttsim.tt_dag_elements import policy_input
 
 @policy_input()
 def ist_hauptberuflich_selbstständig() -> bool:
-    """Self-employed (main profession)."""
+    """Self-employed (main occupation).
+
+    A person is self-employed as a main occupation if the self-employed activity clearly
+    exceeds the other gainful activities in terms of economic significance and time
+    expenditure.
+    """
