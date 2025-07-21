dbc804c6e4f6a068fe18e6adc1fc194bcc7a5792
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-07-21 10:07:11 +0200

    Change custom warnings to 'UserWarning's in order to allow pytest run in parallel, fixes #1022.

--- src/ttsim/interface_dag_elements/warn_if.py
+++ src/ttsim/interface_dag_elements/warn_if.py
@@ -16,56 +16,10 @@ if TYPE_CHECKING:
     import datetime
 
     from ttsim.interface_dag_elements.typing import (
-        OrderedQNames,
         PolicyEnvironment,
         QNameData,
         UnorderedQNames,
     )
-
-
-class FunctionsAndDataColumnsOverlapWarning(UserWarning):
-    """
-    Warning that functions which compute columns overlap with existing columns.
-
-    Parameters
-    ----------
-    columns_overriding_functions : UnorderedQNames
-        Names of columns in the data that override hard-coded functions.
-    """
-
-    def __init__(self, columns_overriding_functions: OrderedQNames) -> None:
-        n_cols = len(columns_overriding_functions)
-        if n_cols == 1:
-            first_part = format_errors_and_warnings("Your data provides the column:")
-            second_part = format_errors_and_warnings(
-                """
-                This is already present among the hard-coded functions of the taxes and
-                transfers system. If you want this data column to be used instead of
-                calculating it within TTSIM you need not do anything. If you want this
-                data column to be calculated by hard-coded functions, remove it from the
-                *data* you pass to TTSIM. You need to pick one option for each column
-                that appears in the list above.
-                """,
-            )
-        else:
-            first_part = format_errors_and_warnings("Your data provides the columns:")
-            second_part = format_errors_and_warnings(
-                """
-                These are already present among the hard-coded functions of the taxes
-                and transfers system. If you want a data column to be used instead of
-                calculating it within TTSIM you do not need to do anything. If you
-                want data columns to be calculated by hard-coded functions, remove them
-                from the *data* you pass to TTSIM. You need to pick one option for
-                each column that appears in the list above.
-                """,
-            )
-        formatted = format_list_linewise(columns_overriding_functions)
-        how_to_ignore = format_errors_and_warnings(
-            """
-            In order to not perform this check, you can ... TODO
-            """,
-        )
-        super().__init__(f"{first_part}\n{formatted}\n{second_part}\n{how_to_ignore}")
 
 
 @warn_function()
@@ -83,39 +37,41 @@ def functions_and_data_columns_overlap(
             and not isinstance(flat_policy_environment.get(col), PolicyInput)
         },
     )
-    if len(overridden_elements) > 0:
-        warnings.warn(
-            FunctionsAndDataColumnsOverlapWarning(overridden_elements),
-            stacklevel=2,
-        )
+    if n_cols := len(overridden_elements) > 0:
+        if n_cols == 1:
+            intro = format_errors_and_warnings("Your data provides the column:")
+            explanation = format_errors_and_warnings(
+                """
+                This is already present among the hard-coded functions of the taxes and
+                transfers system. If you want this data column to be used instead of
+                calculating it within TTSIM you need not do anything. If you want this
+                data column to be calculated by hard-coded functions, remove it from the
+                *data* you pass to TTSIM. You need to pick one option for each column
+                that appears in the list above.
 
+                Turn off warnings by setting `include_warn_nodes=False` in `main`.
+                If you want to be selective about warnings, include these
+                `main_targets`.
+                """,
+            )
+        else:
+            intro = format_errors_and_warnings("Your data provides the columns:")
+            explanation = format_errors_and_warnings(
+                """
+                These are already present among the hard-coded functions of the taxes
+                and transfers system. If you want a data column to be used instead of
+                calculating it within TTSIM you do not need to do anything. If you
+                want data columns to be calculated by hard-coded functions, remove them
+                from the *data* you pass to TTSIM. You need to pick one option for
+                each column that appears in the list above.
 
-class EvaluationDateSetInMultiplePlacesWarning(UserWarning):
-    """
-    Warning that evaluation date is set in multiple places.
-
-    Parameters
-    ----------
-    columns_overriding_functions : UnorderedQNames
-        Names of columns in the data that override hard-coded functions.
-    """
-
-    def __init__(self, conditions: dict[str, bool]) -> None:
-        nicely_formatted_conditions = "\n".join(
-            [f"- {k}" for k, v in conditions.items() if v]
-        )
-        msg = f"""
-You have specified the evaluation date in more than one way:
-
-{nicely_formatted_conditions}
-
-The last of these will be used.
-
-Note that this warnings function does not check for `evaluation_month`
-and `evaluation_day`, never set them anywhere without also setting
-`evaluation_year`.
-"""
-        super().__init__(msg)
+                Turn off warnings by setting `include_warn_nodes=False` in `main`.
+                If you want to be selective about warnings, include these
+                `main_targets`.
+                """,
+            )
+        msg = f"{intro}\n{format_list_linewise(overridden_elements)}\n{explanation}"
+        warnings.warn(UserWarning(msg), stacklevel=2)
 
 
 @warn_function(
@@ -143,7 +99,18 @@ def evaluation_date_set_in_multiple_places(
         "`evaluation_year` is present in the data": "evaluation_year" in processed_data,
     }
     if sum(conditions.values()) > 1:
-        warnings.warn(
-            EvaluationDateSetInMultiplePlacesWarning(conditions),
-            stacklevel=2,
+        nicely_formatted_conditions = "\n".join(
+            [f"- {k}" for k, v in conditions.items() if v]
         )
+        msg = f"""
+You have specified the evaluation date in more than one way:
+
+{nicely_formatted_conditions}
+
+The last of these will be used.
+
+Note that this warnings function does not check for `evaluation_month`
+and `evaluation_day`, never set them anywhere without also setting
+`evaluation_year`.
+"""
+        warnings.warn(UserWarning(msg), stacklevel=2)
