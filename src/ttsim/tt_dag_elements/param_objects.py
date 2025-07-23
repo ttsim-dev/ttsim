205f3585d90f780cd3794ce0cb813ba5d4d757e2
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-07-23 16:15:25 +0200

    Fail if backend has changed (#1046)
    Add `fail_if.backend_has_changed`. 
    
    Lessons learned:
    - Numpy can handle Jax arrays (see test)
    - Jax can handle NumPy arrays that are passed as the processed data (see
    test)
    - The problematic case are parameters that are partialled to functions.
    Unfortunately, these are typically custom objects. We to loop over them and
    check whether any of them happens to be a numpy array

--- src/ttsim/tt_dag_elements/param_objects.py
+++ src/ttsim/tt_dag_elements/param_objects.py
@@ -114,6 +114,13 @@ class ConsecutiveIntLookupTableParam(ParamObject):
 class ConsecutiveIntLookupTableParamValue:
     """The `value` for ConsecutiveIntLookupTable."""
 
+    __slots__ = (
+        "bases_to_subtract",
+        "lookup_multipliers",
+        "values_to_look_up",
+        "xnp",
+    )
+
     bases_to_subtract: Int[Array, "n_rows n_cols"]
     lookup_multipliers: Int[Array, "n_rows n_cols"]
     values_to_look_up: (
--- tests/ttsim/interface_dag_elements/test_failures.py
+++ tests/ttsim/interface_dag_elements/test_failures.py
@@ -11,7 +11,12 @@ import numpy
 import pandas as pd
 import pytest
 
-from ttsim import InputData, MainTarget, main
+try:
+    import jax
+except ImportError:
+    jax = None
+
+from ttsim import InputData, MainTarget, OrigPolicyObjects, TTTargets, main
 from ttsim.interface_dag_elements.fail_if import (
     ConflictingActivePeriodsError,
     _param_with_active_periods,
@@ -1413,6 +1418,85 @@ def test_raise_some_error_without_input_data(
             main_target=MainTarget.results.df_with_mapper,
             backend=backend,
             orig_policy_objects={"root": METTSIM_ROOT},
+        )
+
+
+@pytest.mark.skipif(jax is None, reason="Jax is not installed")
+def test_backend_has_changed_from_jax_to_numpy_passes():
+    policy_environment = main(
+        main_target=MainTarget.policy_environment,
+        policy_date_str="2000-01-01",
+        orig_policy_objects=OrigPolicyObjects(root=METTSIM_ROOT),
+        backend="jax",
+    )
+    input_data = InputData.tree(
+        tree={
+            "p_id": jax.numpy.array([0, 1, 2]),  # type: ignore[union-attr]
+            "property_tax": {
+                "acre_size_in_hectares": jax.numpy.array([5, 20, 200]),  # type: ignore[union-attr]
+            },
+        }
+    )
+    main(
+        main_target=MainTarget.results.df_with_nested_columns,
+        input_data=input_data,
+        policy_environment=policy_environment,
+        tt_targets=TTTargets(tree={"property_tax": {"amount_y": None}}),
+        backend="numpy",
+    )
+
+
+@pytest.mark.skipif(jax is None, reason="Jax is not installed")
+def test_backend_has_changed_from_numpy_for_processed_data_to_jax_passes():
+    input_data = InputData.tree(
+        tree={
+            "p_id": numpy.array([0, 1, 2]),
+            "property_tax": {
+                "acre_size_in_hectares": numpy.array([5, 20, 200]),
+            },
+        }
+    )
+    processed_data = main(
+        main_target=MainTarget.processed_data,
+        backend="numpy",
+        input_data=input_data,
+    )
+    main(
+        main_target=MainTarget.results.df_with_nested_columns,
+        policy_date_str="2000-01-01",
+        orig_policy_objects=OrigPolicyObjects(root=METTSIM_ROOT),
+        input_data=input_data,
+        processed_data=processed_data,
+        tt_targets=TTTargets(tree={"property_tax": {"amount_y": None}}),
+        backend="jax",
+    )
+
+
+@pytest.mark.skipif(jax is None, reason="Jax is not installed")
+def test_backend_has_changed_from_numpy_for_policy_environment_to_jax_raises(
+    xnp: ModuleType,
+):
+    policy_environment = main(
+        main_target=MainTarget.policy_environment,
+        policy_date_str="2000-01-01",
+        orig_policy_objects=OrigPolicyObjects(root=METTSIM_ROOT),
+        backend="numpy",
+    )
+    input_data = InputData.tree(
+        tree={
+            "p_id": xnp.array([0, 1, 2]),
+            "property_tax": {
+                "acre_size_in_hectares": xnp.array([5, 20, 200]),
+            },
+        }
+    )
+    with pytest.raises(ValueError, match="Backend has changed"):
+        main(
+            main_target=MainTarget.results.df_with_nested_columns,
+            input_data=input_data,
+            policy_environment=policy_environment,
+            tt_targets=TTTargets(tree={"property_tax": {"amount_y": None}}),
+            backend="jax",
         )
 
 
