7c53340fb99b501d6583a7659ae9dcee6233bc02
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-07-24 13:38:42 +0200

    Need try/except in version import, after all.

--- src/ttsim/__init__.py
+++ src/ttsim/__init__.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
 
-from ttsim._version import __version__, __version_tuple__, version, version_tuple
+try:
+    # Import the version from _version.py which is dynamically created by
+    # setuptools-scm upon installing the project with pip.
+    # Do not put it under version control!
+    from ttsim._version import __version__, __version_tuple__, version, version_tuple
+except ImportError:
+    __version__ = "unknown"
+    __version_tuple__ = ("unknown", "unknown", "unknown")
+    version = "unknown"
+    version_tuple = ("unknown", "unknown", "unknown")
+
 from ttsim.copy_environment import copy_environment
 from ttsim.interface_dag_elements.shared import merge_trees
 from ttsim.main import main
