c69ccd08d0002b496029d5d9934ae61f442d1b77
Marvin Immesberger <74215010+MImmesberger@users.noreply.github.com>
2025-08-01 08:58:21 +0200

    Remove orphaned nodes from tt dag (#23)
    ### What problem do you want to solve?
    
    The TT DAG has orphan nodes when not selecting a target via
    `NodeSelector`.
    
    - Those nodes are: `evaluation_x`, `policy_x` for `x \in ["year",
    "month", "day", "date"]`
    - They are part of the DAG because they are `policy_inputs`
    - They are orphaned if they are not relevant for other `ColumnObject`s.
    This happens quite often because they usually inform `param_function`s
    (in GETTSIM), not `policy_function`s.
    
    If this is the case, this PR removes the explicitly from the plotting
    DAG.
    
    ---------
    
    Co-authored-by: Hans-Martin von Gaudecker <hmgaudecker@gmail.com>

--- mettsim/middle_earth/housing_benefits/amount.py
+++ mettsim/middle_earth/housing_benefits/amount.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
-from ttsim.tt import policy_function
+from typing import TYPE_CHECKING
+
+from ttsim.tt import param_function, policy_function
+
+if TYPE_CHECKING:
+    from types import ModuleType
+
+    from ttsim.tt import ConsecutiveIntLookupTableParamValue
 
 
 @policy_function(vectorization_strategy="vectorize")
@@ -8,8 +15,18 @@ def amount_m_fam(
     eligibility__requirement_fulfilled_fam: bool,
     income__amount_m_fam: float,
     assistance_rate: float,
+    max_amount_m_fam: float,
+    xnp: ModuleType,
 ) -> float:
     if eligibility__requirement_fulfilled_fam:
-        return income__amount_m_fam * assistance_rate
+        return xnp.minimum(income__amount_m_fam * assistance_rate, max_amount_m_fam)
     else:
         return 0
+
+
+@param_function()
+def max_amount_m_fam(
+    policy_year: int,
+    max_amount_m_fam_by_policy_year: ConsecutiveIntLookupTableParamValue,
+) -> float:
+    return max_amount_m_fam_by_policy_year.look_up(policy_year)
