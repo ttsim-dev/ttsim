975b5ebe5d799c911e0b0d1da44d1e9e19824258
Marvin Immesberger <immesberger@posteo.de>
2025-08-09 16:28:54 +0200

    Fix test.

--- tests/test_plot_dag.py
+++ tests/test_plot_dag.py
@@ -445,11 +445,12 @@ def test_can_pass_plotly_kwargs_to_tt():
 
 
 def test_fail_if_selection_type_is_all_paths_and_less_than_two_primary_nodes():
-    with pytest.raises(ValueError, match="you must provide at least two primary nodes"):
+    with pytest.raises(
+        ValueError, match="you must provide at least two\nprimary nodes"
+    ):
         tt(
             root=middle_earth.ROOT_PATH,
             primary_nodes=["payroll_tax__amount_y"],
             selection_type="all_paths",
-            selection_depth=None,
-            include_params=True,
+            policy_date_str="2025-01-01",
         )
