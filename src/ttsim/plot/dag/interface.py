6a8dc4defe45732a96a5656e0f9c3a7cdc6fa257
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-08-22 05:50:29 +0200

    Actually allow importing colormap for interface DAG in GETTSIM.

--- src/ttsim/plot/dag/interface.py
+++ src/ttsim/plot/dag/interface.py
@@ -22,7 +22,7 @@ if TYPE_CHECKING:
 
     import plotly.graph_objects as go
 
-DEFAULT_COLORMAP = {
+INTERFACE_COLORMAP = {
     ("policy_date",): "gold",
     ("policy_date_str",): "gold",
     ("orig_policy_objects",): "gold",
@@ -57,7 +57,7 @@ def interface(
     show_node_description: bool = False,
     output_path: Path | None = None,
     remove_orig_policy_objects__root: bool = True,
-    node_colormap: dict[tuple[str, ...], str] | None = DEFAULT_COLORMAP,
+    node_colormap: dict[tuple[str, ...], str] | None = INTERFACE_COLORMAP,
     **kwargs: Any,  # noqa: ANN401
 ) -> go.Figure:
     """Plot the full interface DAG.
